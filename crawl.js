0525facf4d2851fdeddc9fa458ad08664dcdfc98
Amelia <alsylvester@hotmail.com>
2023-12-21 21:12:46 -0700

    tests as far as the eye can see

--- crawl.js
+++ crawl.js
@@ -1,12 +1,37 @@
+const {JSDOM} = require('jsdom')
+
 function normalizeURL(url) {
     const urlObj = new URL(url)
-    let fullPath = `${urlObj.host}$(urlObj.pathname)`
+    let fullPath = `${urlObj.host}${urlObj.pathname}`
     if (fullPath.length > 0 && fullPath.slice(-1) === '/') {
         fullPath = fullPath.slice(0, -1)
     }
     return fullPath
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const aEls = dom.window.document.querySelectorAll('a')
+    for (const aEl of aEls) {
+        if (aEl.href.slice(0, 1) === '/') {
+            try {
+                urls.push(new URL(aEl.href, baseURL).href)
+            } catch (err) {
+                console.log(`${err.message}: ${aEl.href}`)
+            }
+        } else {
+            try {
+                urls.push(new URL(aEl.href).href)
+            } catch (err) {
+                console.log(`${err.message}: ${aEl.href}`)
+            }
+        }
+    }
+    return urls
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
